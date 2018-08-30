76147c65dce9300e8ed7e6a46a3ef8a3246b6fcf
William Palermo <william.moraes@gft.com>
2018-08-30 16:27:44 -0300

    eureka e zuul config

--- reserva-facil/taxas-app/src/main/java/br/com/cvc/taxas/Application.java
+++ reserva-facil/taxas-app/src/main/java/br/com/cvc/taxas/Application.java
@@ -2,9 +2,17 @@ package br.com.cvc.taxas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.PropertySource;
 
 @PropertySource(value="file:./application.yml")
+@EnableCircuitBreaker
+@EnableHystrix
+@EnableDiscoveryClient
+@EnableFeignClients
 @SpringBootApplication
 public class Application 
 {
